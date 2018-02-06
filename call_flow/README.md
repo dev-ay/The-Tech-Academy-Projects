29a98a68cb9041727a1f733f4ca91d04bbd7b480
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-02-05 16:26:04 -0800

    Add check-box option, resolve progress bug
    Add user requested features: additional list-item, check-box functionality

--- call_flow/README.md
+++ call_flow/README.md
@@ -5,8 +5,10 @@ I wanted to build a simple browser-based application that a health care customer
 
 ## Steps Taken
 I used the Bootstrap 4 framework for the heading and list items, then a progress bar from W3 schools. In order to have the different list items change color when clicked, I had to write my own JavaScript file. Once I was able to get the list items to change colors, I also built an event listener and an additional JavaScript function to change the style of the progress bar and it's text as the list items are clicked by the user.
+  
+After doing some user testing, there was a request to add an 8th list item. I added this list item and updated the program for all users. Another request was submitted to make a version of the application that uses check boxes so that users could go in their own order instead of just sequentially. This update has been completed in a separate "checks.html" file so that both versions are available. I had to re-write my JavaScript file as well to accommodate the new user input, this is stored as script2.js. I am especially proud of how responsive the new scripting is, updating the progress bar actively while the user is checking and/or unchecking boxes.
 
 ## Results
-The final application is very basic, and has some trouble running in internet explorer. I was able to get it running in Chrome, so for the time being this is where I will direct users. Future improvements might include wider browser support and cleaner styling, but this app meets the basic requirements and is ready for use as is.
+The final application is very basic, and has some trouble running in internet explorer. I was able to get it running in Chrome, so for the time being this is where I will direct users. Future improvements might include wider browser support and cleaner styling. Currently both the check-box version and the list item version meet the project requirements are fully functioning.
   
 *Note: For best functionality, please run this application in Google Chrome.*
--- call_flow/script.js
+++ call_flow/script.js
@@ -1,4 +1,4 @@
-console.log("The custom script is running")
+console.log("The custom script is running");
 
 // Start: Hilight active list item
 var btnContainer = document.getElementById("steps");
@@ -7,44 +7,39 @@ var btns =  btnContainer.getElementsByClassName("list-group-item");
 // this will through the buttons and add the active class to the clicked list item
 for (var i = 0; i < btns.length;  i++) {
     btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
+        var current = document.getElementsByClassName("list-group-item active");
+        current[0].className = current[0].className.replace("list-group-item active", "list-group-item");
         this.className += " active";
     });
 };
-// End: Hilight Active List Item
 
 // Start: update progress bar
 function progress_bar() {
     if (btns[0].className == "list-group-item active") {
-        // console.log("The first button is sellected")
-        document.getElementById("bar").style.width = "14%";
-        document.getElementById("bar").innerHTML = "14%";
+        document.getElementById("bar").style.width = "12%";
+        document.getElementById("bar").innerHTML = "12%";
     } else if (btns[1].className == "list-group-item active") {
-        // console.log("The Second button is sellected")
-        document.getElementById("bar").style.width = "28%";
-        document.getElementById("bar").innerHTML = "28%";
+        document.getElementById("bar").style.width = "25%";
+        document.getElementById("bar").innerHTML = "25%";
     } else if (btns[2].className == "list-group-item active") {
-        // console.log("The third button is sellected")
-        document.getElementById("bar").style.width = "42%";
-        document.getElementById("bar").innerHTML = "42%";
+        document.getElementById("bar").style.width = "37%";
+        document.getElementById("bar").innerHTML = "37%";
     } else if (btns[3].className == "list-group-item active") {
-        // console.log("The fourth button is sellected")
-        document.getElementById("bar").style.width = "56%";
-        document.getElementById("bar").innerHTML = "56%";
+        document.getElementById("bar").style.width = "50%";
+        document.getElementById("bar").innerHTML = "50%";
     } else if (btns[4].className == "list-group-item active") {
-        // console.log("The fifth button is sellected")
-        document.getElementById("bar").style.width = "70%";
-        document.getElementById("bar").innerHTML = "70%";
+        document.getElementById("bar").style.width = "62%";
+        document.getElementById("bar").innerHTML = "62%";
     } else if (btns[5].className == "list-group-item active") {
-        // console.log("The sizth button is sellected")
-        document.getElementById("bar").style.width = "84%";
-        document.getElementById("bar").innerHTML = "84%";
+        document.getElementById("bar").style.width = "75%";
+        document.getElementById("bar").innerHTML = "75%";
     } else if (btns[6].className == "list-group-item active") {
-        // console.log("The seventh button is sellected")
+        document.getElementById("bar").style.width = "87%";
+        document.getElementById("bar").innerHTML = "87%";
+    } else if (btns[7].className == "list-group-item active") {
         document.getElementById("bar").style.width = "100%";
         document.getElementById("bar").innerHTML = "100%";
-    }
+    };
 };
 
-btnContainer.addEventListener("click", progress_bar)
+btnContainer.addEventListener("click", progress_bar);
