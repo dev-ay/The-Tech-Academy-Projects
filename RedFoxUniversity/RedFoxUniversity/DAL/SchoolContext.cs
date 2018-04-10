49571cbc0e3d863af479890aaba322f614d1b150
Joshua Jones <joshjones103@gmail.com>
2018-04-09 22:10:24 -0700

    Update code creating databases
    VS only loads old builds, problemsolving before creating new solution

--- RedFoxUniversity/RedFoxUniversity/DAL/SchoolContext.cs
+++ RedFoxUniversity/RedFoxUniversity/DAL/SchoolContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using RedFoxUniversity.Models;
+﻿using RedFoxUniversity.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
--- RedFoxUniversity/RedFoxUniversity/Models/Enrollment.cs
+++ RedFoxUniversity/RedFoxUniversity/Models/Enrollment.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace RedFoxUniversity.Models
+﻿namespace RedFoxUniversity.Models
 {
     public enum Grade
     {
