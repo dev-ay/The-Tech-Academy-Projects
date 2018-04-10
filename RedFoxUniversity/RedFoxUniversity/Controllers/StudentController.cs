484e67336d6b1e1ae12f03ad16ee17a4d03784c2
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-04-10 09:00:52 -0700

    Fix student details and database

--- RedFoxUniversity/RedFoxUniversity/Controllers/StudentController.cs
+++ RedFoxUniversity/RedFoxUniversity/Controllers/StudentController.cs
@@ -44,10 +44,10 @@ namespace RedFoxUniversity.Controllers
 
         // POST: Student/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,EnrollmentDate")] Student student)
+        public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
             if (ModelState.IsValid)
             {
@@ -76,10 +76,10 @@ namespace RedFoxUniversity.Controllers
 
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,EnrollmentDate")] Student student)
+        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
             if (ModelState.IsValid)
             {
