91dc6fa8d30b92bb10e220ad8a20c2afcd893eed
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-04-13 08:54:40 -0700

    Add sorting, filtering, paging and grouping functionality

--- RedFoxUniversity/RedFoxUniversity/Controllers/HomeController.cs
+++ RedFoxUniversity/RedFoxUniversity/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RedFoxUniversity.DAL;
+using RedFoxUniversity.ViewModels;
 
 namespace RedFoxUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
             return View();
@@ -15,9 +18,19 @@ namespace RedFoxUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                    group student by student.EnrollmentDate into dateGroup
+                    select new EnrollmentDateGroup()
+                    {
+                        EnrollmentDate = dateGroup.Key,
+                        StudentCount = dateGroup.Count()
+                    };
+            return View(data.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         public ActionResult Contact()
